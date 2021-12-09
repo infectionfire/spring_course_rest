44ffc6546690255c12f645058f54ae59407c144b
Infectionfire <infectionfire0911@gmail.com>
2021-12-09 13:04:18 +0300

    Add functional for view one person

--- src/main/java/com/ivanovsergei/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/ivanovsergei/spring/rest/controller/MyRESTController.java
@@ -4,6 +4,7 @@ import com.ivanovsergei.spring.rest.entity.Employee;
 import com.ivanovsergei.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class MyRESTController {
     return allEmployees;
     }
 
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){//с помощью аннтоации передается число в метод и выводится результат
+        Employee employee = employeeService.getEmployee(id);
+
+        return employee;//json получается автоматически при помощи спринга и джексон датабайнд
+    }
 
 
 
