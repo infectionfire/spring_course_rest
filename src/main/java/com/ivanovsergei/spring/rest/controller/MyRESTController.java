70684b7588abd4946baf70f9ca5d30ac4e90d19b
Infectionfire <infectionfire0911@gmail.com>
2021-12-09 14:37:10 +0300

    Create functional to add employee

--- src/main/java/com/ivanovsergei/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/ivanovsergei/spring/rest/controller/MyRESTController.java
@@ -1,13 +1,9 @@
 package com.ivanovsergei.spring.rest.controller;
 
 import com.ivanovsergei.spring.rest.entity.Employee;
-import com.ivanovsergei.spring.rest.exception_handling.EmployeeIncorrectData;
 import com.ivanovsergei.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.ivanovsergei.spring.rest.service.EmployeeService;
-import org.dom4j.io.ElementModifier;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
